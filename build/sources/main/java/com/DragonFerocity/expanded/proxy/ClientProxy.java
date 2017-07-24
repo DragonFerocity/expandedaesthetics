a8f006752e43f6d6bdb3a3b9b71c7f40d917b42a
DragonFerocity <rtanq9@mst.edu>
2017-07-24 09:29:08 -0500

    Removed unnecesary folders

--- build/sources/main/java/com/DragonFerocity/expanded/proxy/ClientProxy.java
+++
@@ -1,9 +0,0 @@
-package com.DragonFerocity.expanded.proxy;
-
-import com.DragonFerocity.expanded.handlers.BlockHandler;
-
-public class ClientProxy extends CommonProxy {
-  public void init() {
-    BlockHandler.registerRenders();
-  }
-}
